e7d13b5af5688ba34d69cc7ec1e1c7e26b547be4
Bill Harper <biharper@cisco.com>
2016-03-09 09:05:39 -0800

    First commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World! How are my People Doing Today !! ";
+    }
+
+public static void main(String[] args) throws Exception {
+     SpringApplication.run(Example.class, args);
+    }
+
+}
