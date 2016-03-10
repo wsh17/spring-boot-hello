10174430da22dcdf63f2e9b46550c8442ba9c98c
Bill Harper <biharper@cisco.com>
2016-03-09 17:07:58 -0800

    changing app name - Bills-Demo2

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World! How are my People Doing Today !! ";
+        return "Hello World! How are my People Doing Today @ Metapod OpenStack !! ";
     }
 
 public static void main(String[] args) throws Exception {
