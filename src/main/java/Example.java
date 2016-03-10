29fba373232d88856bedc264be965a0826aa68ac
Bill Harper <biharper@cisco.com>
2016-03-09 17:58:50 -0800

    recompiled jar file again

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello Metapod World!  Welcome to CiscoLive!! ";
+        return "Hello Metapod World!  Welcome to CiscoLive 2016!! ";
     }
 
 public static void main(String[] args) throws Exception {
