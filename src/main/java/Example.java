9634ecb859bed120237fe426a7678d328cd91937
Bill Harper <biharper@cisco.com>
2016-03-25 16:17:22 -0700

    V7.0

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping(value = "/")
     String home() {
-        return "Hello Metapod 2016 .... Welcome to OpenStack Austin Team Metapod 4.0 !!! ";
+        return "Hello Metapod 2016 .... Welcome to OpenStack Austin Team Metapod 6.0 !!! ";
     }
 
 public static void main(String[] args) throws Exception {
